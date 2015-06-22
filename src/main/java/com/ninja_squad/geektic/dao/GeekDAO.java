c09435a47a13fe102912e78220888f2b030d3b03
MAROUA OTHMAN <MAROUA241@GMAL.COM>
2015-06-22 12:24:30 +0200

    Commit modif GeekDao; add getGeekbyInteret

--- src/main/java/com/ninja_squad/geektic/dao/GeekDAO.java
+++ src/main/java/com/ninja_squad/geektic/dao/GeekDAO.java
@@ -1,11 +1,14 @@
 package com.ninja_squad.geektic.dao;
 
 import java.util.List;
+import java.util.Set;
+
 import javax.persistence.*;
 
 import org.springframework.stereotype.Repository;
 
 import com.ninja_squad.geektic.model.Geek;
+import com.ninja_squad.geektic.model.Interet;
 
 @Repository
 public class GeekDAO {
@@ -19,6 +22,25 @@ public class GeekDAO {
 		query.setParameter("sexe", String.valueOf(sexe));
 		return query.getResultList();
 	}
+	public List<Geek> getGeekBySexe(char sexe) {
+		String jpql = "SELECT geek FROM Geek as geek WHERE geek.sexe=:sexe";
+		TypedQuery<Geek> query = em.createQuery(jpql, Geek.class);
+		query.setParameter("sexe", String.valueOf(sexe));
+		return query.getResultList();
+	}
+	public List<Geek> getGeekByInteret(Set<Interet> interets) {
+		String jpql = "SELECT geek FROM Geek as geek WHERE geek.interets=:interets";
+		TypedQuery<Geek> query = em.createQuery(jpql, Geek.class);
+		query.setParameter("interets", String.valueOf(interets));
+		return query.getResultList();
+	}
+	public List<Geek> getGeekBySexeAndInteret(char sexe,Set<Interet> interets) {
+		String jpql = "SELECT geek FROM Geek as geek WHERE geek.sexe=:sexe and geek.interets=:interets";
+		TypedQuery<Geek> query = em.createQuery(jpql, Geek.class);
+		query.setParameter("sexe", String.valueOf(sexe));
+		return query.getResultList();
+	}
+
 
 	public Geek getGeekById(int id) {
 		return em.find(Geek.class, id);
