2ebb9f465c1faa5220335212f1866a1c25b22340
MAROUA OTHMAN <MAROUA241@GMAL.COM>
2015-06-22 12:56:00 +0200

    route accueil

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,7 +1,10 @@
+
 var app = angular.module("geektic", ['ngRoute']);
 
-app.controller('HelloCtrl', function($scope, $http) {
-    $http.get('/api/hello').success(function(helloMessage) {
-        $scope.helloMessage = helloMessage;
-    });
+app.config(function($routeProvider){
+    
+    $routeProvider
+    .when('/', {
+        templateUrl: '/views/accueil.html'})     
 });
+
